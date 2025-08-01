4930d168e91ef756c4474bc28c1fbfe6725db2e1
Sam-2019 <samtmartey@gmail.com>
2025-08-01 14:26:42 +0000

    minor adjustment

--- src/components/vehicle.jsx
+++ src/components/vehicle.jsx
@@ -9,12 +9,18 @@ const Specification = ({ vehicle }) => {
     <div className="p-7 rounded-3xl bg-gray-200 space-y-3">
       <div className="flex flex-row justify-between items-center">
         <p className="text-2xl font-semibold">Vehicle specifications</p>
-        <div className="text-2xl font-semibold">
+        <div className="text-2xl font-semibold hidden md:block">
           No. <span className="text-xs font-normal">{vehicle.number}</span>
         </div>
       </div>
 
       {/* <p className="text-sm">Specifications</p> */}
+      {vehicle.number && (
+        <div className="grid grid-cols-2 gap-4 md:hidden">
+          <span className="text-sm">No.:</span>
+          <p className="text-sm ">{vehicle.number}</p>
+        </div>
+      )}
 
       {vehicle.matriculationYear && (
         <div className="grid grid-cols-2 gap-4">
@@ -55,28 +61,43 @@ const Contact = () => {
       </p>
 
       <div className="flex flex-row flex-wrap space-x-3 space-y-3">
-        <Button asChild variant="outline" size="lg" className='w-full md:w-auto'>
+        <Button
+          asChild
+          variant="outline"
+          size="lg"
+          className="w-full md:w-auto"
+        >
           <NavLink to="#" className="flex flex-row items-center">
             <Phone size={23} />
             <p className="text-sm">Call us</p>
           </NavLink>
         </Button>
 
-        <Button asChild variant="outline" size="lg" className='w-full md:w-60'>
+        <Button asChild variant="outline" size="lg" className="w-full md:w-60">
           <NavLink to="#" className="flex flex-row items-center">
             <MessageCircle size={23} />
             <p className="text-sm">Chat with us</p>
           </NavLink>
         </Button>
 
-        <Button asChild variant="outline" size="lg" className='w-full md:w-auto'>
+        <Button
+          asChild
+          variant="outline"
+          size="lg"
+          className="w-full md:w-auto"
+        >
           <NavLink to="#" className="flex flex-row items-center">
             <Mail size={23} />
             <p className="text-sm">Email us</p>
           </NavLink>
         </Button>
 
-        <Button asChild variant="outline" size="lg" className='w-full md:w-auto'>
+        <Button
+          asChild
+          variant="outline"
+          size="lg"
+          className="w-full md:w-auto"
+        >
           <NavLink to="#" className="flex flex-row items-center">
             <img src={whatappSvg} alt="whatsapp" width="20" />
             <p className="text-sm"> Send a Whatsapp message</p>
